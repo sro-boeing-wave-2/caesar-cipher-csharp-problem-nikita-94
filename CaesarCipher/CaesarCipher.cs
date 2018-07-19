6b74cd4499ffbdb754727013165fe258cc9db02d
Nikita karanpuria <nikita.karanpuria8@gmail.com>
2018-07-19 19:27:55 +0530

    done

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,14 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
+            return string.Join("", text.ToCharArray().Select(ch => RotateChar(ch, shiftKey)));
             throw new NotImplementedException("You need to implement this function.");
+        }
+        private static char RotateChar(char ch, int shiftKey)
+        {
+            if (!char.IsLetter(ch)) return ch;
+            char _base = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((ch - _base + shiftKey) % 26 + _base);
         }
     }
 }
